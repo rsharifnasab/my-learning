09ecb660a1a7e2ce564c962f56877ef7f6c7a502
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-04-15 16:19:34 +0430

    added java7 functional interface

---
+++ OOP/java8/1-default-method-interface/J8Test.java
@@ -0,0 +1,28 @@
+import java.time.*;
+
+interface Java8Interface {
+    boolean todayIsSaturday();
+    default boolean todayIsNotSaturday(){
+       return ! todayIsSaturday();
+    }
+}
+
+class Java8class implements Java8Interface {
+
+    public boolean todayIsSaturday() {
+        DayOfWeek dow = LocalDate.now().getDayOfWeek();
+        if(dow == DayOfWeek.SATURDAY) return true;
+        else return false;
+    }
+
+    // no need to anything about todayIsNotSaturday method
+}
+
+
+public class J8Test {
+    public static void main(String[] args) {
+        Java8Interface j8i = new Java8class();
+        System.out.println(j8i.todayIsSaturday());
+        System.out.println(j8i.todayIsNotSaturday());
+    }
+}
