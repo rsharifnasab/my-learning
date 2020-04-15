cfc903281644dbf83896da41600f6ed24d102ae3
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-04-15 16:37:13 +0430

    added functional interface

--- OOP/java8/2-functional-interface/J8Test.java
+++ OOP/java8/2-functional-interface/J8Test.java
@@ -1,30 +1,34 @@
 import java.time.*;
 
-interface Java7Interface {
+@FunctionalInterface
+interface Java8Interface { 
+
+
     boolean todayIsSaturday();
-    boolean todayIsNotSaturday();
+    // just one un-implemented method
+
+
+    default boolean todayIsNotSaturday(){
+        return !todayIsSaturday();
+    } // use another unimplemented method
+
+
 }
 
 
-public class J7Test {
+public class J8Test {
     public static void main(String[] args) {
 
 
-        Java7Interface j7i = new Java7Interface(){
-            public boolean todayIsSaturday() {
-                DayOfWeek dow = LocalDate.now().getDayOfWeek();
-                if(dow == DayOfWeek.SATURDAY) return true;
-                else return false;
-            }
-            public boolean todayIsNotSaturday(){
-                return !todayIsSaturday();
-            }
-        }; // define mathods while instancing
+        Java8Interface j8i = () ->  LocalDate.now().getDayOfWeek() == DayOfWeek.SATURDAY;
+         // define the only method while instancing
 
-        System.out.println(j7i.todayIsSaturday());
-        System.out.println(j7i.todayIsNotSaturday());
+        System.out.println(j8i.todayIsSaturday());
+        System.out.println(j8i.todayIsNotSaturday());
     }                                        
 
 
 }
+
+
 
---
+++ OOP/java8/2-functional-interface/README.md
@@ -0,0 +1,13 @@
+## before java7
+while instancing a function, you should define all methods 
+
+
+## after java8
+in java 8, functional interface added. 
+functional interface is an interface that:
++ all of methods have deafult implementation except one 
++ could be instanced with implementing just one method 
+
+
+
+further reading [this link](https://www.geeksforgeeks.org/functional-interfaces-java/)
