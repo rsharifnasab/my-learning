62be2cb5e60b8dc81adad604593af1ea32aeb605
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-04-15 17:54:06 +0430

    add functions and lambda examples  from java uti function

---
+++ OOP/java8/2-functional-interface/UtilFunctions.java
@@ -0,0 +1,28 @@
+import java.util.function.*;
+
+public class UtilFunctions {
+    public static void main(String[] args) {
+        
+
+        Predicate<String> salamHast = (a) -> a.equals("salam");
+        if( salamHast.test("some other string") ) 
+            System.out.println("test mosbat");
+        
+        if( salamHast.negate().test("another string") )
+            System.out.println("test manfi");
+
+         //////////////////////////////////////
+
+        Function<Integer,String> intToString1 = a -> UtilFunctions.customToString(a);
+        System.out.println(intToString1.apply(1));
+
+        // refrnce to method (same as pointer to function)
+        Function<Integer,String> intToString2 = UtilFunctions::customToString;
+        System.out.println(intToString2.apply(2));
+    }
+
+
+    static String customToString(Integer inp){
+         return "this is your string {" + inp + "} in demical";
+    }
+}
