004ab74a3ff59dec1e0af00b030378bddd9037f7
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-07-23 15:33:23 +0430

    added server client program to java

---
+++ OOP/socket/README.md
@@ -0,0 +1,32 @@
+## simple client server program with Java
+
+this is an chat application
+
+### in server-side
+
+1. compile server program:
+
+   ```bash
+   javac server/*.java
+   ```
+
+2. run server with:
+   ```bash
+   java server.Server
+   ```
+3. connect exactly `2` clients
+
+
+### in client-side
+1. compile client program
+   ```bash
+   javac client/*java
+   ```
+2.  run client with: 
+   ```bash
+   java client.Client
+   ```
+   
+3. enter server IP address
+4. connect another client 
+5. enjoy
---
+++ OOP/socket/server/ClientHandler.java
@@ -0,0 +1,51 @@
+package server;
+
+import java.net.*;
+import java.io.*;
+
+public class ClientHandler extends Thread{
+    private final Socket socket;
+    private final DataInputStream   in;
+    private final DataOutputStream out;
+    public final int number;
+
+    public ClientHandler(Socket s, int no) throws Exception{
+        this.socket = s;
+        this.in  = new DataInputStream(s.getInputStream());
+        this.out = new DataOutputStream(s.getOutputStream());
+
+        this.number = no;
+        System.out.println("client "+number+" connected");
+
+        send(""+number); // send client number
+    }
+
+
+    // extending Thread
+    // after staring thread, this method will run 
+    // concurrently
+    public void run(){
+        try{
+            String data = "";
+            while(!data.equals("done")){
+                data = in.readUTF();
+                System.out.println(number+":"+data);
+
+
+                // send message to toher clients
+                for(ClientHandler cl : server.Server.connections){
+                    if(cl == this) continue; // dont send for ourself!
+                    cl.send("from "+number+": "+data);
+                }
+            }
+        } catch(Exception e){
+            throw new RuntimeException("client handle of "+number+"had problems",e);
+        }
+    }
+
+    public synchronized void send(String toSend)throws IOException {
+        out.writeUTF(toSend);
+    }
+}
+
+
