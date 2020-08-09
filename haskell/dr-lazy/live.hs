cbd21afaf6d87b71992eea64451537243c8688e0
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-08-10 03:27:50 +0430

    added live.hs

---
+++ haskell/dr-lazy/live.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE TypeApplications #-}
+
+data Person = Hadi | DrLazy | JediMahdi 
+    deriving (Show)
+
+memptyIfFalse :: Monoid a => a -> Bool -> a
+memptyIfFalse a avail = if avail then a else mempty
+
+optionalPeople :: Bool -> [Person]
+optionalPeople = memptyIfFalse [ Hadi ]
+
+defaultPeople :: [Person]
+defaultPeople = [ DrLazy, JediMahdi ]
+
+main :: IO ()
+main = do
+    putStrLn "is Hadi available?"
+    isHadiAvailable <- readLn @Bool 
+    print $ optionalPeople isHadiAvailable <> defaultPeople
