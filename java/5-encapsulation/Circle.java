8555583a772c96e334e3fdace5a295a21c31ea8b
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-02-24 10:53:10 +0330

    java re indent

--- java/5-encapsulation/Circle.java
+++ java/5-encapsulation/Circle.java
@@ -1,33 +1,33 @@
-public class Circle{
+public class Circle {
 
-  private double area;
-  private double r;
-  private double p;
+    private double area;
+    private double r;
+    private double p;
 
-  public Circle(double r){
-    this.r = r;
-    this.area =  Math.PI * r * r;
-    this.p = 2 * r * Math.PI;
-  }
+    public Circle(double r) {
+        this.r = r;
+        this.area =  Math.PI * r * r;
+        this.p = 2 * r * Math.PI;
+    }
 
-  public double getArea(){
-    return this.area;
-  }
+    public double getArea() {
+        return this.area;
+    }
 
-  public double getR(){
-    return this.r;
-  }
+    public double getR() {
+        return this.r;
+    }
 
-  public double getP(){
-    return this.p;
-  }
+    public double getP() {
+        return this.p;
+    }
 
-  public static void main(String[] args) {
-    Circle c1 = new Circle(1);
-    System.out.println(c1.getR());
-    System.out.println(c1.getArea());
-    System.out.println(c1.getP());
-  }
+    public static void main(String[] args) {
+        Circle c1 = new Circle(1);
+        System.out.println(c1.getR());
+        System.out.println(c1.getArea());
+        System.out.println(c1.getP());
+    }
 }
 
 /*
