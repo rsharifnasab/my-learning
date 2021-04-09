90eb0087ebf33f1f9ce804f86b529842c36bf92c
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-04-09 21:48:42 +0430

    format java code

--- java/exception/A.java
+++ java/exception/A.java
@@ -1,18 +1,18 @@
 
 public class A {
-    static void f(){
+    static void f() {
         try {
             throw new Exception("1");
-        } catch(Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("2", e);
         }
     }
-    public static void main(String[] args) throws Exception{
-        try{
+    public static void main(String[] args) throws Exception {
+        try {
             f();
 
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             e.getCause().printStackTrace();
         }
